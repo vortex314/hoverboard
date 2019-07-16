14007d3c32cfd062d39632e50488b92dd84ffa74
Robo Durden <roland@robosoft.de>
2019-07-16 14:31:33 +0200

    Update comms.c

--- src/comms.c
+++ src/comms.c
@@ -57,7 +57,7 @@ void consoleScope() {
   #endif
 
   #if defined DEBUG_SERIAL_ASCII && (defined DEBUG_SERIAL_USART2 || defined DEBUG_SERIAL_USART3)
-    memset((void*)uart_buf, 0, sizeof(uart_buf));	//ROBO
+    memset((void*)uart_buf, 0, sizeof(uart_buf));	//ROBO 
     sprintf((void*)uart_buf, "1:%i 2:%i 3:%i 4:%i 5:%i 6:%i 7:%i 8:%i\r\n", ch_buf[0], ch_buf[1], ch_buf[2], ch_buf[3], ch_buf[4], ch_buf[5], ch_buf[6], ch_buf[7]);	//ROBO
 
     if(UART_DMA_CHANNEL->CNDTR == 0) {
